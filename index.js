afbb39b7d4e59151fe7244edc849a2500a59d279
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-15 17:56:26 +0700

    Use session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var multer = require('multer');
+var session = require('express-session');
 
 var greetings = require('./greetings/index.js');
 
@@ -11,11 +12,19 @@ app.use(bodyParser.urlencoded({ extended: false })); // To parse URL encoded dat
 app.use(bodyParser.json()); // To parse request json -> req.body
 app.use(cookieParser()); // To parse cookie json -> req.cookies
 app.use(multer().array()); // To parse multipart/form-data
+app.use(session({secret: `s3cr3t`}));
 
 app.use('/public', express.static('public'));
 
 app.get('/', function(req, res) {
-  res.send('Welcome to Group Management');
+  let views = 1;
+  if(req.session.page_views){
+    req.session.page_views++;
+    views = req.session.page_views;
+  } else {
+    req.session.page_views = 1;
+  }
+  res.send(`Welcome to Group Management: ${views}`);
 });
 
 app.use('/greetings', greetings);
