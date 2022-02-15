710a1b034ce5f18768f4a2cbbc93e72bbb62c205
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-15 16:16:43 +0700

    Use Multer form data parse

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var multer = require('multer');
 
 var greetings = require('./greetings/index.js');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // To parse URL encoded data
-app.use(bodyParser.json()); // To parse request json
+app.use(bodyParser.json()); // To parse request json -> req.body
 app.use(cookieParser()); // To parse cookie json -> req.cookies
+app.use(multer().array()); // To parse multipart/form-data
 
 app.use('/public', express.static('public'));
 
