5028a2322c8b43402c9c7e7573142aff5115fba6
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-15 16:08:04 +0700

    Use BodyParser and CookieParser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var greetings = require('./greetings/index.js');
+
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false })); // To parse URL encoded data
+app.use(bodyParser.json()); // To parse request json
+app.use(cookieParser()); // To parse cookie json -> req.cookies
 
 app.get('/', function(req, res) {
   res.send("Welcome to Group Management");
