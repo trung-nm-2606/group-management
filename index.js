c9353e0760c433e955c62e8b27f1388e16888789
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-16 10:56:55 +0700

    Demo multipart form

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var multer = require('multer');
 var session = require('express-session');
+
+var Uploaders = require('./uploaders');
 
 var greetings = require('./greetings/index.js');
 var users = require('./users/index.js');
@@ -15,10 +16,17 @@ app.set('views','./views');
 app.use(bodyParser.urlencoded({ extended: false })); // To parse URL encoded data
 app.use(bodyParser.json()); // To parse request json -> req.body
 app.use(cookieParser()); // To parse cookie json -> req.cookies
-app.use(multer().array()); // To parse multipart/form-data
 app.use(session({secret: `s3cr3t`}));
 
 app.use('/public', express.static('public'));
+app.use('/uploads', express.static('uploads'));
+
+// This is only for demo
+app.get('/demo-upload', (req, res) => res.render('demo-upload'));
+app.post('/demo-upload', Uploaders.Image.single('image'), (req, res) => {
+  console.log(req.body.name);
+  res.redirect('/demo-upload');
+});
 
 app.get('/', function(req, res) {
   let views = 1;
