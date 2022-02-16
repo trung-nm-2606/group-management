c0bc969d1ceb09b902f91169ab415e5a1f44c781
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-16 21:29:02 +0700

    Update App with auth checking at client side

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,46 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Nav from './Nav';
 
+const pingAuth = () => axios.get('/api/users/auth-ping');
+
 const App = () => {
+  const [auth, setAuth] = useState(false);
+
   useEffect(() => {
-    axios.get('/api/users/auth-ping')
+    const intervalId = setInterval(() => {
+      pingAuth()
+        .then(res => {
+          const authenticated = res.data;
+          if (!authenticated) {
+            window.location.href = '/login';
+          } else {
+            setAuth(true);
+          }
+        });
+    }, 30 * 1000);
+
+    console.log('App bootstrapped!!!');
+    pingAuth()
       .then(res => {
         const authenticated = res.data;
         if (!authenticated) {
-          // window.location.href = '/login';
+          window.location.href = '/login';
+        } else {
+          setAuth(true);
         }
-      })
+      });
+
+    return () => {
+      console.log('App terminated!!!');
+      if (intervalId) {
+        clearInterval(intervalId);
+        console.log('Clear ping auth');
+      }
+    };
   }, []);
+
+  if (!auth) return 'Bootstrapping...'
 
   return (
     <>
