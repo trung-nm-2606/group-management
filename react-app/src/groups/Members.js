ad50dc40eee3a7a7a619fb9d1b4ee13e0c145f4c
trung-nm-2606 <trung.nm.2606@gmail.com>
2022-02-17 14:12:26 +0700

    Add remove member

--- react-app/src/groups/Members.js
+++ react-app/src/groups/Members.js
@@ -12,12 +12,15 @@ import Input from '@mui/material/Input';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { Grid } from '@mui/material';
 import Alert from '@mui/material/Alert';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 const Members = () => {
   const [members, setMembers] = useState([]);
   const [newMemberEmail, setNewMemberEmail] = useState(null);
   const [addingMember, setAddingMember] = useState(false);
-  const [addingMemberError, setAddingMemberError] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
@@ -43,17 +46,36 @@ const Members = () => {
             .then(res => setMembers(res.data))
           ;
         } else {
-          setAddingMemberError(error);
+          setError(error);
         }
         setAddingMember(false);
       })
     ;
-  }, [newMemberEmail, setAddingMember, setMembers, setNewMemberEmail, setAddingMemberError]);
+  }, [newMemberEmail, setAddingMember, setMembers, setNewMemberEmail, setError]);
+
+  const onRemoveMember = useCallback((userPk) => {
+    axios
+      .post('/api/groups/4/remove_members', { memberPks: [userPk] })
+      .then((res) => {
+        const { oper = {} } = res.data;
+        const { status, error } = oper;
+        if (status) {
+          axios
+            .get('/api/groups/4/members')
+            .then(res => setMembers(res.data))
+          ;
+        } else {
+          setError(error);
+        }
+        setAddingMember(false);
+      })
+    ;
+  }, [setAddingMember, setMembers, setError]);
 
   return (
     <>
-      {addingMemberError && (
-        <Alert severity="error" sx={{ marginBottom: 1 }}>{addingMemberError}</Alert>
+      {error && (
+        <Alert severity="error" sx={{ marginBottom: 1 }}>{error}</Alert>
       )}
       <Grid container mb={1} alignItems="center" justifyContent="flex-start" direction="row">
         <Grid item mr={1}>
@@ -77,6 +99,7 @@ const Members = () => {
               <TableCell>#</TableCell>
               <TableCell>Full name</TableCell>
               <TableCell>Email</TableCell>
+              <TableCell>Operation</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -93,6 +116,20 @@ const Members = () => {
                 </Typography>
                 </TableCell>
                 <TableCell>{email}</TableCell>
+                <TableCell>
+                  <Tooltip title="Remove member">
+                    <IconButton
+                      color="error"
+                      aria-label="remove member"
+                      component="span"
+                      onClick={() => {
+                        onRemoveMember(pk);
+                      }}
+                    >
+                      <DeleteOutlineIcon />
+                    </IconButton>
+                  </Tooltip>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
